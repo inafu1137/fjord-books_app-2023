b44ebcf905307a86ef4c39e4261792f5b398c560
inafu1137 <inafu1137@gmail.com>
2025-05-15 13:29:21 +0900

    Userモデルの実装実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_one_attached :icon
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validate :icon_format
+
+  private
+
+  def icon_format
+    return unless icon.attached?
+
+    return if icon.content_type.in?(%w[image/jpeg image/png image/gif])
+
+    errors.add(:icon, 'はjpg, png, gif形式のみアップロード可能です')
+  end
 end
