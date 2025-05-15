cfbb89980f09569cfbc127c5b758671ab925bb35
inafu1137 <inafu1137@gmail.com>
2025-05-15 13:27:14 +0900

    Usersコントローラーの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,25 @@
 
 class UsersController < ApplicationController
   def index
-    @users = User.order(:id).page(params[:page])
+    @users = User.includes(icon_attachment: :blob).order(:id).page(params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: 'プロフィール情報を更新しました'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :postal_code, :address, :self_introduction, :icon)
   end
 end
