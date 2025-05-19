046b8f7cc6a9475fb48f50c9875a478ce72c3516
inafu1137 <inafu1137@gmail.com>
2025-05-19 12:02:06 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  devise_for :users
   root to: 'books#index'
-  resources :books
-  resources :users, only: %i(index show)
+
+  devise_for :users
+  resources :users, only: %i[index show]
+
+  resources :books do
+    resources :comments, only: %i[create edit update destroy], shallow: true
+  end
+
+  resources :reports do
+    resources :comments, only: %i[create edit update destroy], shallow: true
+  end
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
