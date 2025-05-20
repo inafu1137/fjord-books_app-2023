60f46c1238f4dae180bfaa0d04ca54fc8e98a5bb
inafu1137 <inafu1137@gmail.com>
2025-05-20 10:08:35 +0900

    インデントの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|ja/ do
-  resources :books
+    resources :books
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
