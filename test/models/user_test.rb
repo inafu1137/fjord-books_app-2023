76bae6a95d6f90f572ac07a1ba8b388a802ceae9
inafu1137 <inafu1137@gmail.com>
2025-06-04 15:09:35 +0900

    userモデルのテストコードを実装

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :users
+
+  test '#name_or_email returns name if present' do
+    user = users(:user_one)
+    assert_equal 'userone', user.name_or_email
+  end
+
+  test '#name_or_email returns email if name is blank' do
+    user = users(:user_two)
+    assert_equal user.email, user.name_or_email
+  end
 end
